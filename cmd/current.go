819b1dfacdcc5b2b0ab26427fef705d18216e133
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-12-13 03:53:07 +0700

    add current command

--- cmd/current.go
+++ cmd/current.go
@@ -1,21 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/abbit/naruw/internal"
 	"github.com/spf13/cobra"
 )
 
-func newLastCommand() *cobra.Command {
+func newCurrentCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "last",
-		Short: "Get information about last watched episode",
-		Long: `This command allows to get information about the last Naruto anime episode you watched,
+		Use:   "current",
+		Short: "Get information about current episode",
+		Long: `This command allows to get information about the current Naruto anime episode you would watch,
 including its title, episode number, and whether it is a canon or filler episode.`,
 		Args: cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			episode, err := internal.GetNarutoLastWatchedEpisode()
+			episode, err := internal.GetNarutoCurrentEpisode()
 			cobra.CheckErr(err)
 
+			fmt.Println("Current Naruto episode:")
 			internal.PrintEpisode(&episode)
 		},
 	}
--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/abbit/naruw/internal"
 	"github.com/abbit/naruw/internal/config"
 	"github.com/spf13/cobra"
 )
@@ -15,16 +14,12 @@ func newRootCommand() *cobra.Command {
 		Short: "CLI for managing Naruto anime episodes",
 		Long: `This CLI app allows to get information about Naruto episodes, 
 including whether the episode is a canon or filler episode,
-get last watched episode and mark episodes as watched on Shikimori.
-
-If called without any arguments, it will print information about the current episode.`,
+get last watched episode and mark episodes as watched on Shikimori.`,
 		Args: cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			episode, err := internal.GetNarutoCurrentEpisode()
-			cobra.CheckErr(err)
-
-			fmt.Println("Current Naruto episode:")
-			internal.PrintEpisode(&episode)
+			if err := cmd.Help(); err != nil {
+				fmt.Println(err)
+			}
 		},
 	}
 
@@ -32,6 +27,7 @@ If called without any arguments, it will print information about the current epi
 	cmd.AddCommand(newInfoCommand())
 	cmd.AddCommand(newLastCommand())
 	cmd.AddCommand(newNextCommand())
+	cmd.AddCommand(newCurrentCommand())
 
 	return cmd
 }
